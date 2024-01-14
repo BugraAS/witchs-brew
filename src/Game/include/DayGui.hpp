c6a1d84ea7f8e37151c5ddbe3209c485194a7ce6
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-14 17:33:45 +0300

    Comment of DayGui.hpp

--- src/Game/include/DayGui.hpp
+++ src/Game/include/DayGui.hpp
@@ -1,14 +1,22 @@
 #pragma once
+
 #include "GuiNode.hpp"
 #include "raylib.h"
 #include <vector>
 
-class DayGui: public GuiNode{
-    private:
-    std::vector<::Rectangle> boxes;
-    public:
+// DayGui class is derived from GuiNode, representing the graphical user interface for the daytime.
+class DayGui : public GuiNode {
+private:
+    std::vector<::Rectangle> boxes; // Vector to store rectangles used for drawing GUI elements.
+
+public:
+    // Constructor for the DayGui class.
     DayGui();
+
+    // Function to display the daytime GUI, implementing the virtual function from the base class.
     virtual void display(Matrix transform) override;
-    private:
+
+private:
+    // Function to transform the rectangles based on a given matrix.
     std::vector<::Rectangle> transformBoxes(Matrix m);
 };
