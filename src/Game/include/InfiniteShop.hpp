b92d274cfb7abc88f63f2c7d305433e4f827e4a4
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-14 17:36:34 +0300

    Comment of InfiniteShop.hpp

--- src/Game/include/InfiniteShop.hpp
+++ src/Game/include/InfiniteShop.hpp
@@ -4,12 +4,18 @@
 #include <string>
 #include <vector>
 
+// InfiniteShop class is derived from TradeAgent, representing a shop with infinite stock.
 class InfiniteShop : public TradeAgent {
 private:
+    // No private members declared in this class.
 
 public:
-    InfiniteShop()=default;
+    // Default constructor for the InfiniteShop class.
+    InfiniteShop() = default;
 
+    // Function to add an item to the shop with infinite stock, a specified price, name, and description.
     void AddItem(Item upgrade, float price, const std::string name, std::string desc);
+
+    // Function to remove an item from the shop based on the provided upgrade.
     void RemoveItem(Item upgrade);
 };
