f9258b1e1607ff693235424d2979b1c89370185d
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-14 17:37:35 +0300

    Comment of NightGui.hpp

--- src/Game/include/NightGui.hpp
+++ src/Game/include/NightGui.hpp
@@ -1,14 +1,22 @@
 #pragma once
+
 #include "GuiNode.hpp"
 #include "raylib.h"
 #include <vector>
 
-class NightGui: public GuiNode{
-    private:
-    std::vector<::Rectangle> boxes;
-    public:
+// NightGui class is derived from GuiNode, representing the graphical user interface for the nighttime.
+class NightGui : public GuiNode {
+private:
+    std::vector<::Rectangle> boxes; // Vector to store rectangles used for drawing GUI elements.
+
+public:
+    // Constructor for the NightGui class.
     NightGui();
+
+    // Function to display the nighttime GUI, implementing the virtual function from the base class.
     virtual void display(Matrix transform) override;
-    private:
+
+private:
+    // Function to transform the rectangles based on a given matrix.
     std::vector<::Rectangle> transformBoxes(Matrix m);
 };
