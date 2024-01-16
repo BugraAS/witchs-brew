404e596280eba6068148fcc5c342b2df0222a61f
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-16 15:02:34 +0300

    Doxygen Customer.hpp

--- src/Game/include/Customer.hpp
+++ src/Game/include/Customer.hpp
@@ -1,29 +1,52 @@
+/**
+ * @file Customer.hpp
+ * @brief Header file for the Customer class.
+ */
+
 #pragma once
 
 #include <string>
 
-// Customer class represents a customer with a name and money for purchasing items.
+/**
+ * @class Customer
+ * @brief Represents a customer with a name and money for purchasing items.
+ */
 class Customer {
 private:
-    std::string name; // Name of the customer.
-    float money;      // Amount of money the customer has.
+    std::string name; ///< Name of the customer.
+    float money;      ///< Amount of money the customer has.
 
     // Other member variables if needed can be added here.
 
 public:
-    // Constructor for the Customer class, initializes the customer with a given name.
+    /**
+     * @brief Constructor for the Customer class.
+     * @param _name The name of the customer.
+     */
     Customer(const std::string& _name);
 
-    // Function to get the name of the customer.
+    /**
+     * @brief Function to get the name of the customer.
+     * @return The name of the customer.
+     */
     const std::string& GetName() const;
 
-    // Function to get the amount of money the customer has.
+    /**
+     * @brief Function to get the amount of money the customer has.
+     * @return The amount of money the customer has.
+     */
     float GetMoney() const;
 
-    // Function to set the amount of money for the customer.
+    /**
+     * @brief Function to set the amount of money for the customer.
+     * @param amount The new amount of money for the customer.
+     */
     void SetMoney(float amount);
 
-    // Function to simulate a purchase by deducting the specified amount from the customer's money.
+    /**
+     * @brief Function to simulate a purchase by deducting the specified amount from the customer's money.
+     * @param amount The amount to deduct from the customer's money.
+     */
     void Purchase(float amount);
 
     // Other methods for customer interactions can be added here.
