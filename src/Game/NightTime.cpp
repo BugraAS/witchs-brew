9bb72e66aa643cfc449ee8bfc35a723ce20f6f5c
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-16 14:36:55 +0300

    Doxygen Comments NightTime.cpp

--- src/Game/NightTime.cpp
+++ src/Game/NightTime.cpp
@@ -1,3 +1,8 @@
+/**
+ * @file NightTime.cpp
+ * @brief Implementation of the NightTime class.
+ */
+
 #include "NightTime.hpp"
 #include "Event.hpp"
 #include "EventQueue.hpp"
@@ -5,33 +10,47 @@
 #include "PotionShop.hpp"
 #include <iostream>
 
-// Constructor for the NightTime class, takes a PotionShop pointer as a parameter.
+/**
+ * @brief Constructor for the NightTime class.
+ * @param shop A pointer to the PotionShop associated with the NightTime instance.
+ */
 NightTime::NightTime(PotionShop* shop)
-: shop(shop), startDay(501)
+    : shop(shop), startDay(501)
 {
-
 }
 
-// Empty process function, needs implementation based on the class's requirements.
+/**
+ * @brief Empty process function.
+ * @details This function needs implementation based on the class's requirements.
+ */
 void NightTime::process(){
     return;
 }
 
-// Function to end the night, fires the startDay event, disables the nighttime, and makes it invisible.
+/**
+ * @brief Function to end the night.
+ * @details Fires the startDay event, disables the nighttime, and makes it invisible.
+ */
 void NightTime::endNight(){
     EventQueue::fireEvent(startDay, 0); // Fires the startDay event with a delay of 0.
     disable(); // Disables the nighttime functionality.
     makeInvisible(); // Makes the nighttime invisible.
 }
 
-// Initialization function for the NightTime class, takes a pointer to void as a parameter.
+/**
+ * @brief Initialization function for the NightTime class.
+ * @details Takes a pointer to void as a parameter.
+ * @param data A pointer to void containing data for initialization.
+ */
 void NightTime::Init(void* data){
     Event startNight = *((Event*)data); // Casts the void pointer to an Event pointer.
+    
     // Lambda function to handle the startNight event, enables the nighttime and makes it visible.
     EventHandler handle = [this](void* x)-> bool{
         this->enable(); // Enables the nighttime functionality.
         this->makeVisible(); // Makes the nighttime visible.
         return false; // The handler is not self-removing.
     };
+    
     EventQueue::addHandler(startNight, handle); // Adds the handler to the EventQueue for the startNight event.
 }
