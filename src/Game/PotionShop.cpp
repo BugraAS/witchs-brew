5bd436b867a207646e668c9f3e09ebe71a9c840f
Baharnur Türkoğlu <154628354+BaharnurTurkoglu@users.noreply.github.com>
2024-01-16 14:42:17 +0300

    Doxygen Comments PotionShop.cpp

--- src/Game/PotionShop.cpp
+++ src/Game/PotionShop.cpp
@@ -1,17 +1,31 @@
+/**
+ * @file PotionShop.cpp
+ * @brief Implementation of the PotionShop class.
+ */
+
 #include "PotionShop.hpp"
 #include "Inventory.hpp"
 #include <algorithm>
 #include <cmath>
 #include <limits>
 
-// Constructor for PotionShop, initializes balance and sets up initial money in inventory
+/**
+ * @brief Constructor for PotionShop.
+ * @param balance The initial balance for the potion shop.
+ * @details Initializes balance and sets up initial money in the inventory.
+ */
 PotionShop::PotionShop(float balance)
-: PotionShop()
+    : PotionShop()
 {
     inventory.addItem(Item::MONEY, balance);
 }
 
-// Method to buy an upgrade for the PotionShop
+/**
+ * @brief Method to buy an upgrade for the PotionShop.
+ * @param upgrade The upgrade to be bought.
+ * @details Checks if the upgrade has already been bought, saves the current money balance,
+ * attempts to buy the upgrade, and adds the bought upgrade to the list of upgrades.
+ */
 void PotionShop::buyUpgrade(Item upgrade){
     // Check if the upgrade has already been bought
     if(inventory.getItemAmount(upgrade) == 1.0)
@@ -34,12 +48,21 @@ void PotionShop::buyUpgrade(Item upgrade){
     upgrades.push_back(upgrade);
 }
 
-// Method to buy a specified amount of ingredient from the supplier
+/**
+ * @brief Method to buy a specified amount of ingredient from the supplier.
+ * @param ingredient The ingredient to be bought.
+ * @param amount The amount of the ingredient to be bought.
+ */
 void PotionShop::buyIngredient(Item ingredient, float amount){
     buy(ingredientSupplier.get(), ingredient, amount);
 }
 
-// Method to craft a potion using ingredients from the inventory
+/**
+ * @brief Method to craft a potion using ingredients from the inventory.
+ * @details Initializes the amount of potion to craft to a very large value, iterates through each
+ * ingredient required for the potion, calculates the maximum amount of potion that can be crafted
+ * based on ingredient availability, and deducts the used amounts from the inventory.
+ */
 void PotionShop::craftPotion(){
     // Initialize the amount of potion to craft to a very large value
     float amount = std::numeric_limits<float>::max();
@@ -60,7 +83,11 @@ void PotionShop::craftPotion(){
     inventory.addItem(Item::POTION, amount);
 }
 
-// Method to uncraft a potion, converting it back into ingredients
+/**
+ * @brief Method to uncraft a potion, converting it back into ingredients.
+ * @details Gets the amount of potion in the inventory, iterates through each ingredient required
+ * for the potion, and adds the corresponding amount of each ingredient back to the inventory.
+ */
 void PotionShop::uncraftPotion(){
     // Get the amount of potion in the inventory
     float amount = inventory.getItemAmount(Item::POTION);
